77ece07e1d37d519795b839f1e2e6c39b608357c
Eran <eranrosines@gmail.com>
2024-11-01 16:47:09 -0600

    values fix attempt

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -30,7 +30,7 @@ class CarControllerParams:
       self.STEER_DRIVER_ALLOWANCE = 250
       self.STEER_DRIVER_MULTIPLIER = 2
       self.STEER_THRESHOLD = 250
-      self.STEER_DELTA_UP = 4 if vEgoRaw < 10. else 2
+      self.STEER_DELTA_UP = 4 if vEgoRaw < 11. else 2
       self.STEER_DELTA_DOWN = 10 if vEgoRaw < 11. else 3
 
     # To determine the limit for your car, find the maximum value that the stock LKAS will request.
